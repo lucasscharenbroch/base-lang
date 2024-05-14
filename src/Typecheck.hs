a6371c7c4b54252e48b8e388510ed700179375f5
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-05-14 08:50:29 -0500

    Add missing join in type-checking for assignments

--- src/Typecheck.hs
+++ src/Typecheck.hs
@@ -6,12 +6,16 @@ import Resolve (R, T)
 import Text.Parsec (SourcePos)
 import Data.List (intercalate)
 import Data.Functor (void)
+import Control.Monad (join)
 
 type CheckM = Either Error
 
-expectType :: SourcePos -> Type -> Type -> CheckM ()
+expectType_ :: SourcePos -> Type -> Type -> CheckM ()
+expectType_ = ((.) . (.) . (.)) void expectType
+
+expectType :: SourcePos -> Type -> Type -> CheckM Type
 expectType pos t t'
-    | t == t' = return ()
+    | t == t' = return t
     | otherwise = Left $ "Type error: expected `" ++ show t ++ "`, got `" ++ show t' ++ "` @ " ++ show pos
 
 expectOneOf :: SourcePos -> [Type] -> Type -> CheckM ()
@@ -36,21 +40,21 @@ checkLvalue (Identifier _pos _id (t, _loc)) = return t
 checkLvalue (TupleAccess _pos _lval _id (t, _loc)) = return t
 
 checkStmt :: Type -> Stmt R T -> CheckM ()
-checkStmt _ (Inc pos lval) = checkLvalue lval >>= expectType pos (TValType VTInteger)
-checkStmt _ (Dec pos lval) = checkLvalue lval >>= expectType pos (TValType VTInteger)
-checkStmt _ (Read pos lval) = checkLvalue lval >>= expectType pos (TValType VTInteger)
+checkStmt _ (Inc pos lval) = checkLvalue lval >>= expectType_ pos (TValType VTInteger)
+checkStmt _ (Dec pos lval) = checkLvalue lval >>= expectType_ pos (TValType VTInteger)
+checkStmt _ (Read pos lval) = checkLvalue lval >>= expectType_ pos (TValType VTInteger)
 checkStmt _ (Write pos expr) = checkExpr expr >>= expectOneOf pos [TValType VTInteger, TValType VTLogical, TValType VTString]
 checkStmt _ (ExprStmt _pos expr) = void $ checkExpr expr
 checkStmt retType (IfElse pos cond body maybeBody) = do
     condType <- checkExpr cond
-    expectType pos (TValType VTLogical) condType
+    expectType_ pos (TValType VTLogical) condType
     _ <- checkBody retType body
     mapM_ (checkBody retType) maybeBody
 checkStmt retType (While pos cond body) = do
     condType <- checkExpr cond
-    expectType pos (TValType VTLogical) condType
+    expectType_ pos (TValType VTLogical) condType
     void $ checkBody retType body
-checkStmt retType (Return pos (Just expr)) = checkExpr expr >>= expectType pos retType
+checkStmt retType (Return pos (Just expr)) = checkExpr expr >>= expectType_ pos retType
 checkStmt retType (Return pos Nothing)
     | retType == TVoid = return ()
     | otherwise = Left $ "Expected return value of type `" ++ show retType ++ "` (got nothing) @ " ++ show pos
@@ -59,7 +63,7 @@ checkExpr :: Expr R -> CheckM Type
 checkExpr (LogicalLit _ _) = return $ TValType VTLogical
 checkExpr (IntLit _ _) = return $ TValType VTInteger
 checkExpr (StringLit _ _) = return $ TValType VTString
-checkExpr (Assignment pos lval expr) = expectType pos <$> checkLvalue lval <*> exprType >> exprType
+checkExpr (Assignment pos lval expr) = join $ expectType pos <$> checkLvalue lval <*> exprType
     where exprType = checkExpr expr
 checkExpr (Call pos lval args) = do
     lvalType <- checkLvalue lval
